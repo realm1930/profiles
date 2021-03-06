7fb573e88358f5dde92e268ca8710b00a8a50c86
Arne <a.vaneycken@gmail.com>
2021-03-06 12:37:38 +0100

    Initial commit

---
+++ src/test/java/be/vdab/profiles/ProfilesApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.profiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfilesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
